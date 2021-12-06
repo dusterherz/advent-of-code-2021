0ff230a3444df669f77ac8de879d21ca7b649308
Aude Lejuez <aude@derniercri.io>
2021-12-06 22:06:32 +0100

    Cleaner day 5

--- day5/__main__.py
+++ day5/__main__.py
@@ -19,35 +19,33 @@ vents = parse_data(
 )
 
 
+def get_step(start, end):
+    if start < end:
+        return 1
+    if start > end:
+        return -1
+    return 0
+
+
 def get_overlapping_vents(part1=True):
     diagram = [[0 for j in range(max_size)] for i in range(max_size)]
     for vent in vents:
-        low_x = min(vent["start"]["x"], vent["end"]["x"])
-        max_x = max(vent["start"]["x"], vent["end"]["x"])
-        low_y = min(vent["start"]["y"], vent["end"]["y"])
-        max_y = max(vent["start"]["y"], vent["end"]["y"])
+        # Skip diagonals vent in part 1
         if (
             vent["start"]["x"] != vent["end"]["x"]
             and vent["start"]["y"] != vent["end"]["y"]
+            and part1
         ):
-            if part1:
-                continue
-            else:
-                step_y = 1 if vent["start"]["y"] < vent["end"]["y"] else -1
-                step_x = 1 if vent["start"]["x"] < vent["end"]["x"] else -1
-                y = vent["start"]["y"]
-                x = vent["start"]["x"]
-                while x != vent["end"]["x"] or y != vent["end"]["y"]:
-                    diagram[y][x] += 1
-                    y += step_y
-                    x += step_x
-                diagram[y][x] += 1
-        elif vent["start"]["x"] != vent["end"]["x"]:
-            for i in range(low_x, max_x + 1):
-                diagram[vent["start"]["y"]][i] += 1
-        else:
-            for i in range(low_y, max_y + 1):
-                diagram[i][vent["start"]["x"]] += 1
+            continue
+        step_x = get_step(vent["start"]["x"], vent["end"]["x"])
+        step_y = get_step(vent["start"]["y"], vent["end"]["y"])
+        y = vent["start"]["y"]
+        x = vent["start"]["x"]
+        while x != vent["end"]["x"] or y != vent["end"]["y"]:
+            diagram[y][x] += 1
+            x += step_x
+            y += step_y
+        diagram[y][x] += 1
     flatten_diagram = sum(diagram, [])
     filtered_diagram = list(filter(lambda x: x >= 2, flatten_diagram))
     return len(filtered_diagram)
